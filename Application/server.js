7b78c06f112e7941155a470d4960e33d93addbf5
andreyyec <andreyyec@gmail.com>
2017-04-14 21:09:02 -0600

    Front end files

---
+++ Application/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, (req, res) => {
+  console.log('listening on 3000')
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/charts', (req, res) => {
+  res.sendFile(__dirname + '/charts.html');
+});
+
+app.get('/tables', (req, res) => {
+  res.sendFile(__dirname + '/tables.html');
+});
+
+app.use('/public', express.static('public'));
+
+app.get('*', function(req, res){
+  res.sendFile('/404.html');
+});
